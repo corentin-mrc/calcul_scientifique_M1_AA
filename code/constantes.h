b754bb030fe1df4ab7769f3f69319bbe0e844b63
MARCOU Corentin <corentin.marcou@efrei.net>
2023-01-07 20:15:40 +0100

    question 3

---
+++ code/constantes.h
@@ -0,0 +1,5 @@
+double a = 10;
+double b = 5;
+double epsilon = 1;
+double gama = 1;
+double lambda = 1;
--- code/main.cpp
+++ code/main.cpp
@@ -4,8 +4,13 @@
 
 using namespace std;
 
+double fsecond_membre(double (*ug) (double), double (*ud) (double), double (*ugpp) (double), double (*udpp) (double), double x, double y)
+{
+	return (-epsilon * (a - x) * ugpp(y) - epsilon * (a + x) * udpp(y) - gama * ug(y) + gama * ud(y) + lambda * (a - x) * ug(y) + lambda * (a + x) * ud(y)) / (2 * a);
+}
+
 int main()
 {
-	cout << "Hello World!" << endl;
+	cout << "Hello World! " << a << " , " << b << " , " << epsilon << " , " << gama << " , " << lambda << endl;
 	return 0;
 }
